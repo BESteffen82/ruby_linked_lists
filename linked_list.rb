db8261934a557aff8c0b2d846924d22a454dd29e
Brent Steffen <BESteffen82@gmail.com>
2022-06-26 17:51:17 -0400

    Add insert_at method

--- linked_list.rb
+++ linked_list.rb
@@ -61,11 +61,11 @@ class LinkedList
 	end
 	
 	def head		
-		p @head.value	
+		@head.value	
 	end
 
 	def tail
-		p @tail.value
+		@tail.value
 	end
 
 	def at(index)
@@ -99,9 +99,42 @@ class LinkedList
 		return includes_value
 	end
 
-	
+	def find(value)
+		current_node = @head
+		node_index = 0
+		until current_node.next_node == nil
+			current_node = current_node.next_node
+			node_index += 1
+			if value == current_node.value
+			return node_index
+			end
+		end
+	end
+
+	def to_s
+		current_node = @head		
+		until current_node == nil			
+			print "(#{current_node.value}) -> "
+			current_node = current_node.next_node
+		end
+	end
+
+	def insert_at(value, index)		
+		current_node = @head
+		node_index = 0		
+		until current_node.next_node == nil
+			current_node = current_node.next_node			
+			if node_index == index - 2				
+				inserted_node = Node.new(value)
+				inserted_node.next_node = current_node.next_node
+				current_node.next_node = inserted_node																
+			end
+			node_index += 1
+		end
+	end
 end
 
+#test procedures
 linked_list = LinkedList.new
 linked_list.prepend("Kona")
 linked_list.append("Buster")
@@ -109,11 +142,18 @@ linked_list.append("Maggie")
 linked_list.prepend("Bella")
 linked_list.append("Bailey")
 linked_list.prepend("Rosebud")
-binding.pry
+p linked_list.to_s
 p linked_list.size
 p linked_list.head
 p linked_list.tail
 p linked_list.at(3)
 p linked_list.pop
+p linked_list.to_s
 p linked_list.size
 p linked_list.contains?("Buster")
+p linked_list.find("Bella")
+linked_list.insert_at("Taffy", 2)
+p linked_list.to_s
+p linked_list.at(2)
+p linked_list.size
+
